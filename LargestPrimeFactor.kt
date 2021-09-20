c06edae14592c4625e91baf77ee8930f7cce9bed
navneeth-ravi <70211287+navneeth-ravi@users.noreply.github.com>
2021-09-20 18:19:13 +0530

    Update LargestPrimeFactor.kt

--- LargestPrimeFactor.kt
+++ LargestPrimeFactor.kt
@@ -1,9 +1,11 @@
 package `Problem-Solving-Kotlin`
+import java.util.Scanner
 fun main(){
     print("\tLargest Prime Factor Finder \nEnter Number:")
-    var num= readLine()!!.toLong()
-    var max:Int?=0
-    max?.plus(2)
+    var inp=Scanner(System.`in`)
+    var num= inp.nextLong()
+    var max:Int=0
+    max.plus(2)
     while((num%2).toInt()==0){
         num=num/2
         max=2
@@ -11,7 +13,7 @@ fun main(){
     for(i in 3..Math.sqrt(num.toDouble()).toInt()step 2)
         while ((num%i).toInt()==0){
             num=num/i
-            if(i>max!!)max=i
+            if(i>max)max=i
         }
     println("ans: $max")
 }
