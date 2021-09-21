577876f5c8fe62b29e8aff54cfcf00a5290302be
navneeth-ravi <70211287+navneeth-ravi@users.noreply.github.com>
2021-09-21 10:45:46 +0530

    Update SmallestMultiple.kt

--- SmallestMultiple.kt
+++ SmallestMultiple.kt
@@ -1,16 +1,16 @@
 package `Problem-Solving-Kotlin`
 fun check(number:Int):Boolean{
-    var checkList: MutableList<Int> =mutableListOf<Int>(18,11,13,17,18,20,14)
+    var checkList: MutableList<Int> =mutableListOf<Int>(18,11,13,17,18,20,14,16,19)
     for(i in checkList) if(number.rem(i)!=0)return false
     return true
 }
 fun solution(limit:Int):Int{
-    for(i in limit..Int.MAX_VALUE step 20 ){
+    for(i in limit..999999999 step limit){
         if(check(i))return i
     }
     return -1
 }
 fun main(){
-    var result= solution(20)
+    var result= solution(10)
     println(result)
 }
