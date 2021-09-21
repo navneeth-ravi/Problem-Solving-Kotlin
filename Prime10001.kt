27158110d33bd9073ac3984213b7dfb170a65a80
navneeth-ravi <70211287+navneeth-ravi@users.noreply.github.com>
2021-09-21 18:13:38 +0530

    Create Prime10001.kt

---
+++ Prime10001.kt
@@ -0,0 +1,22 @@
+package `Problem-Solving-Kotlin`
+
+fun main(){
+    val result=10001
+    var count=2
+    var isPrime:(Int)->Boolean= {
+        var result:Boolean=true
+        for (i in 3..it - 1) if(it % i == 0){
+            result=false
+            break
+        }
+        result
+    }
+    for(i in 3..Int.MAX_VALUE step 2)
+        if(isPrime(i)) {
+            if (count == 10001) {
+                println("ans: $i")
+                break
+            }
+            count += 1
+        }
+}
